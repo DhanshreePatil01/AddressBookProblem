1e0a823b46644c8c75e5a4ddc99a8d322bcb62b3
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-11-21 23:41:20 +0530

    Added :UC1 Contact is created in Address Book.

---
+++ src/com/Bridgelabz/AddressBook.java
@@ -0,0 +1,19 @@
+package com.Bridgelabz;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		
+		Contact contact1=new Contact();
+		contact1.setFirstName("Dhanshree");
+		contact1.setLastName("Patil");
+		contact1.setAddress("Patil Villa");
+		contact1.setCity("Latur");
+		contact1.setState("Maharashtra");
+		contact1.setEmail("patilds@gmail.com");
+		contact1.setZipCode(413512);
+		contact1.setpNumber(797293612);
+		System.out.println(contact1);
+		
+	}
+}
