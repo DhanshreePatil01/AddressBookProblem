9569dc52444e2af7d069eee4bdfe4fa742ccae82
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-11-21 23:47:36 +0530

    Added :UC2 Ability to add new Contacts in Address Book.

--- src/com/Bridgelabz/AddressBook.java
+++ src/com/Bridgelabz/AddressBook.java
@@ -1,19 +1,76 @@
 package com.Bridgelabz;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class AddressBook {
 
-	public static void main(String[] args) {
+	Scanner input=new Scanner(System.in);
 		
-		Contact contact1=new Contact();
-		contact1.setFirstName("Dhanshree");
-		contact1.setLastName("Patil");
-		contact1.setAddress("Patil Villa");
-		contact1.setCity("Latur");
-		contact1.setState("Maharashtra");
-		contact1.setEmail("patilds@gmail.com");
-		contact1.setZipCode(413512);
-		contact1.setpNumber(797293612);
-		System.out.println(contact1);
+		Contact array[]=new Contact[10] ;
+		int indexCount=0;
 		
-	}
+		void pushData(Contact contactobj)
+		{
+			this.array[indexCount]=contactobj;
+			indexCount++;
+		}
+
+		void dynamicMathod(){
+			boolean waitTillSomeOneExit=true;
+			
+			do
+			{
+				System.out.println("Enter the option you want to choose: ");
+				System.out.println(" 1.Add Contact \n 2.Exit");
+				int option=input.nextInt();
+				
+				switch(option)
+				{
+				case 1:
+					addContact();
+					break;
+					
+				case 2:
+					waitTillSomeOneExit=false;
+					System.out.println("Exit");
+				}
+			}
+			while(waitTillSomeOneExit);
+		}
+		void addContact(){
+			
+			System.out.println("Enter First Name: ");
+			String firstName=input.next();
+			
+			System.out.println("Enter Last Name: ");
+			String lastName=input.next();
+			
+			System.out.println("Enter Address: ");
+			String address=input.next();
+			
+			System.out.println("Enter city: ");
+			String city=input.next();
+			
+			System.out.println("Enter state: ");
+			String state=input.next();
+			
+			System.out.println("Enter Email: ");
+			String email=input.next();
+			
+			System.out.println("Enter Zip code: ");
+			int zipCode=input.nextInt();
+			
+			System.out.println("Enter phone number: ");
+			long pNumber=input.nextLong();
+			
+			Contact person=new Contact(firstName,lastName,address,city,state,email,zipCode,pNumber);
+			pushData(person);
+		}
+		@Override
+		public String toString() {
+			return "AddressBook [array=" + Arrays.toString(array) + "]";
+		}
+		
+	
 }
---
+++ src/com/Bridgelabz/AddressBookMain.java
@@ -0,0 +1,15 @@
+package com.Bridgelabz;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		
+		
+		AddressBook family=new AddressBook();
+		family.dynamicMathod();
+	
+	   System.out.println(family);
+  }
+}
